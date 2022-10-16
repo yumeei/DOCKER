c069da37464e69bdd974f12d9b94b05427e12fc1
yumei <yumei@yumeei.lan>
2022-10-17 01:35:54 +0200

    message system

--- app/post.php
+++ app/post.php
@@ -1,51 +1,63 @@
 <?php
 session_start();
+
 $pdo = new PDO("mysql:host=database;dbname=data", "root", "password");
-if (isset($_POST['valider'])){
-    if(!empty($_POST['content'])){
-//        faire en sorte que le username soit celui lors de la creation
-        $username = htmlspecialchars($_SESSION['username']);
-        $content= nl2br(htmlspecialchars($_POST['content']));
 
-        $inserercontent= $pdo->prepare('INSERT INTO post (username, content) VALUES(?, ?)');
-        $inserercontent->execute(array($username, $content));
-    }else{
-        echo "veuillez compléter tous les champs";
+if (isset($_SESSION['id']) and !empty($_SESSION['id'])) {
+    if (isset($_POST['validate'])) {
+        if (isset($_POST['content']) and !empty($_POST['content'])) {
+            $username = htmlspecialchars($_SESSION['username']);
+            $content = htmlspecialchars($_POST['content']);
+
+            $user_id = $pdo->prepare('SELECT id FROM user WHERE username = ?');
+            $user_id->execute(array($username));
+            $user_id = $user_id->fetch();
+            $user_id = $user_id['id'];
+            $ins = $pdo->prepare('INSERT INTO post(user_id, content) VALUES (?,?)');
+            $ins->execute(array($_SESSION['id'], $content));
+
+            $error = "Message sent successfully";
+        } else {
+            $error = "Please fill all the fields";
+        }
     }
-}
+
 ?>
+    <!DOCTYPE html>
+    <html lang="en">
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta username="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Instant messages</title>
-</head>
-<body>
-    <h1>Hello, user</h1>
-<a href="logout.php">Log out</a>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
 
-<form method="POST" action="">
-    <textarea name="content"></textarea>
-    <input type="submit" name="validate">
-</form>
-<section id="content">
-    <?php
-    $pdo = new PDO("mysql:host=database;dbname=data", "root", "password");
-    $recupcontent= $pdo->query('SELECT * FROM post');
-    while($content = $recupcontent->fetch()){
+    <body>
+        <?php 
+        $pdo = new PDO("mysql:host=database;dbname=data", "root", "password");
+        $getContent = $pdo->query('SELECT * FROM post, user');
         ?>
-            <h4><?= $content['username']; ?></h4>
+        <h1>Hello, <?= $_SESSION['username']; ?> !</h1>
+        <a href="logout.php">Log out</a>
+        <form method="POST" action="">
+            <textarea name="content"></textarea>
+            <input type="submit" name="validate">
+            <?php if (isset($error)) {
+                echo $error;
+            } ?>
+        </form>
+        <?php
+        while ($content = $getContent->fetch()) {
+        ?>
+            <h4>Message from <?= $content['username']; ?></h4>
             <p><?= $content['content']; ?></p>
         <?php
-    }
-    ?>
-</section>
+        }
+        ?>
 
+    </body>
 
+    </html>
 
-</body>
-</html>
+<?php } ?>
