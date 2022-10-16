8b8813477a99d0318b8f0e018947a60450f3a7e4
yumei <yumei@yumeei.lan>
2022-10-16 21:07:29 +0200

    edit

--- app/post.php
+++ app/post.php
@@ -22,7 +22,7 @@ if (isset($_POST['valider'])){
     <meta username="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>contentrie instantané</title>
+    <title>Instant messages</title>
 </head>
 <body>
     <h1>Hello, user</h1>
